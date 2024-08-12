d263b065b3924acacd1f423465552120a692e37b
CanerYesiltas <caneryesiltas1@gmail.com>
2024-08-12 19:50:25 -0400

    starting to build api fetch function

---
+++ coin-app-starter/main.js
@@ -0,0 +1,37 @@
+//? MAIN.JS
+
+const CoinInput = document.querySelectorAll("#input");
+const CoinBtn = document.querySelectorAll("btn");
+
+
+
+
+
+
+
+
+
+function getCoinApis () {
+
+
+    const CoinRankApiKey = "coinrankingfd080161bf3c6e9f143e6efa86956b64e891529ed7b1315c";
+  
+    const CoinRankUrl = `https://api.coinranking.com/v2/coins?api_key=${CoinRankApiKey}&offset=0&limit=10`;
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+CoinSearch
+
+CoinBtn.addEventListener('click' , CoinSearch );
+
